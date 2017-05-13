c540ecb3794662ec3382eebdab9686dd512553cf
fry767 <michaeldeslauriers.md@gmail.com>
2017-05-13 16:21:52 -0400

    added some macro for the protocol lib

--- SW4STM32/BootloaderV1/Application/User/Bootloader_Settings.h
+++ SW4STM32/BootloaderV1/Application/User/Bootloader_Settings.h
@@ -1,5 +1,13 @@
 #include "com_interface.h"
-
+#include "bootloaderhw.h"
 #define START_FLASH_ADDRESS 0x08000000
 #define BOOTLOADER_SIZE 20
+
+
 #define COMMUNICATION_RECEIVE_FUNCTION(value,size)  Receive_Char_In_IT_Mode(value,size)
+#define COMMUNICATION_SEND_FUNCTION(ptr_ch,size)    Send_Char(ptr_ch,size)
+
+
+#define STORE_IN_EEPROM_FUNCTION(rcv_value)		    Bootloader_WriteStorage32(rcv_value);
+#define READ_IN_MEMORY(Address)					    Bootloader_Read(Address)
+#define GET_STORAGE_ADDRESS()						Bootloader_GetStorageAddress();
