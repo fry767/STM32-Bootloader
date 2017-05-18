c52a2b3ed98f7f752d5930b494ee7fcddf42a508
fry767 <michaeldeslauriers.md@gmail.com>
2017-05-17 21:57:43 -0400

    - Change to the send function to accept uint8_t* instead of char*. I dont think its necessary

--- SW4STM32/BootloaderV1/Application/User/Bootloader_Settings.h
+++ SW4STM32/BootloaderV1/Application/User/Bootloader_Settings.h
@@ -1,11 +1,11 @@
 #include "com_interface.h"
 #include "bootloaderhw.h"
 #define START_FLASH_ADDRESS 0x08000000
-#define BOOTLOADER_SIZE 20
+#define BOOTLOADER_SIZE 30
 
 
 #define COMMUNICATION_RECEIVE_FUNCTION(value,size)  Receive_Char_In_IT_Mode(value,size)
-#define COMMUNICATION_SEND_FUNCTION(ptr_ch,size)    Send_Char(ptr_ch,size)
+#define COMMUNICATION_SEND_FUNCTION(ptr,size)    Send_Buf8(ptr,size)
 
 
 #define STORE_IN_EEPROM_FUNCTION(rcv_value)		    Bootloader_WriteStorage32(rcv_value);
